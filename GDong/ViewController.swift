5fd98937ccf7a723394099a8f7914b88f6c28ba2
Juan Chen <juanchen917@gmail.com>
2016-04-05 15:54:16 -0700

    add border styles to textFields

--- GDong/ViewController.swift
+++ GDong/ViewController.swift
@@ -9,10 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var phoneNumberTextField: UITextField!
+    
+    @IBOutlet weak var sendVerificationCode: UIButton!
+    
+    @IBOutlet weak var textVerificationTextField: UITextField!
+        
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    func addTextFieldBorder(textField: UITextField){
+        let border = CALayer()
+        let width = CGFloat(1.2)
+        border.borderColor = UIColor.lightGrayColor().CGColor
+        border.frame = CGRect(x: 0, y: textField.frame.size.height - width, width:  textField.frame.size.width, height: textField.frame.size.height)
+
+        border.borderWidth = width
+        textField.layer.addSublayer(border)
+        textField.layer.masksToBounds = true
+    }
+    
+    
+    override func viewDidLayoutSubviews() {
+        
+        addTextFieldBorder(phoneNumberTextField)
+        addTextFieldBorder(textVerificationTextField)
+        addTextFieldBorder(passwordTextField)
+        
+
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
